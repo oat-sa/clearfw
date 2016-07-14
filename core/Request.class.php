384c67745dabdfddd5cefdc277c2c164109bb9f9
Ivan Klimchuk <klimchuk@1pt.com>
2016-07-14 15:31:28 +0300

    Added checks for avoiding notices using application in cli mode

--- core/Request.class.php
+++ core/Request.class.php
@@ -162,17 +162,17 @@ class Request
  	
  	public function getUserAgent()
  	{
- 		return $_SERVER['USER_AGENT'];
+		return isset($_SERVER['USER_AGENT']) ? $_SERVER['USER_AGENT'] : '';
  	}
  	
  	public function getQueryString()
  	{
- 		return $_SERVER['QUERY_STRING'];
+		return isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '';
  	}
  	
  	public function getRequestURI()
  	{
- 		return $_SERVER['REQUEST_URI'];
+ 		return isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
  	}
         
         /**
